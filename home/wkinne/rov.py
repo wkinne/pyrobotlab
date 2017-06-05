d5b8e50bcf42423d8f2267eef2c300156022bf61
wkinne <kinneplastics@yahoo.com>
2017-06-05 16:31:34 -0400

    Update rov.py

--- home/wkinne/rov.py
+++ home/wkinne/rov.py
@@ -10,6 +10,8 @@ arduino.connect("COM4")
 rightMotor = Runtime.createAndStart("rightMotor", "Motor")
 leftMotor = Runtime.createAndStart("leftMotor", "Motor")
 rearMotor = Runtime.createAndStart("rearMotor", "Motor")
+xservo = Runtime.createAndStart("xservo","Servo")
+yservo = Runtime.createAndStart("yservo","Servo")
  
 # create the joystick service
 joystick = Runtime.createAndStart("joystick", "Joystick")
@@ -18,6 +20,12 @@ joystick = Runtime.createAndStart("joystick", "Joystick")
 rearMotor.setPwrDirPins( 3, 4)
 rightMotor.setPwrDirPins( 7, 8)
 leftMotor.setPwrDirPins( 5, 6)
+
+xservo.attach(arduino,26)
+yservo.attach(arduino,28)
+
+xservo.map(-1,1,0,180)
+yservo.map(-1,1,0,180)
 
 # attach the motors to the arduino so they can be controlled
 rightMotor.attach(arduino)
@@ -45,10 +53,17 @@ def onJoystickInput(data):
   update = False
   # a debug statement to print the data being returned from the joystick
   print(data, data.id, data.value)
+  
+  # ry & rx run the camera gimbal  
+  
   if data.id == u'ry':
      print("button ry is ", data.value)
-  if data.id == u'rx':
+  if data.id == 'ry':
+     yservo.moveTo(data.value)
+  if data.id == 'rx':
      print("button rx is ", data.value)
+  if data.id == u'rx':
+     xservo.moveTo(data.value)
   if data.id == u'x':
      # assign the value to the global variable "x"
      x = data.value
@@ -58,7 +73,10 @@ def onJoystickInput(data):
      # assign the joystick value to the global variable y
      y = data.value
      update = True
-     # print("button y is ", data.value)
+  
+  # z runs the rear motor, pick up the rear of the ROV and move forward to dive
+  #                        lower the rear of the ROV and move forward to surface
+  
   if data.id == 'z':
      print("axis z is ", data.value)
   if data.id == 'z':
@@ -66,10 +84,12 @@ def onJoystickInput(data):
      
   # at this point we have the latest values for x and y saved in global memory
   # compute the values to send to the motors
+  
   if update:
-    lefty=y+x                  
-    righty=y-x
-    # send the values to the motors.
-    leftMotor.move(lefty)
-    rightMotor.move(righty)
+     lefty=y+x             
+     righty=y-x
  
+     # send the values to the motors.
+  
+     leftMotor.move(lefty)
+     rightMotor.move(righty)
